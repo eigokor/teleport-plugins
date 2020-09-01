23582bd36a3893da3f94a42963a1cb2161ddb916
Vladimir Kochnev <marshall-lee@evilmartians.com>
2020-09-01 18:22:39 +0300

    gitlab: Enable go test -race.

--- access/gitlab/app.go
+++ access/gitlab/app.go
@@ -42,6 +42,11 @@ func (a *App) Run(ctx context.Context) error {
 	return trace.Wrap(a.mainJob.Err())
 }
 
+// Err returns the error app finished with.
+func (a *App) Err() error {
+	return trace.Wrap(a.mainJob.Err())
+}
+
 // WaitReady waits for http and watcher service to start up.
 func (a *App) WaitReady(ctx context.Context) (bool, error) {
 	return a.mainJob.WaitReady(ctx)
@@ -285,6 +290,10 @@ func (a *App) onWebhookEvent(ctx context.Context, hook Webhook) error {
 	pluginData, err := a.getPluginData(ctx, reqID)
 	if err != nil {
 		return trace.Wrap(err)
+	}
+
+	if pluginData.GitlabData.ID == 0 {
+		return trace.Errorf("plugin data is empty")
 	}
 
 	if pluginData.GitlabData.ID != issueID {
